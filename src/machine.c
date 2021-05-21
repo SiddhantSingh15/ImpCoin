4fd05ba86a7b7d48394b105b15d438f92187d0c4
Ashvin Arsakularatne <aa9220@ic.ac.uk>
2021-05-22 00:32:30 +0530

    chore: Adds struct for machine state

--- src/machine.c
+++ src/machine.c
@@ -1,1 +1,12 @@
+#include <stdint.h>
+
 // For the core structs relating to the emulator
+
+typedef struct pipeline_t {
+} pipeline_t;
+
+typedef struct machine_state_t {
+  uint8_t main_memory[65535];
+  uint8_t register_file[16];
+  pipeline_t *pipeline;
+} machine_state_t;

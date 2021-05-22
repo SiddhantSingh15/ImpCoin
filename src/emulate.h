b00ac572267c5f69c8f2652ae498233e8f5dca1b
Ashvin Arsakularatne <aa9220@ic.ac.uk>
2021-05-22 19:23:43 +0530

    refactor: rename types to avoid clash, refactor instruction_t

--- src/emulate.h
+++ src/emulate.h
@@ -2,8 +2,7 @@
 #define EMULATE_H
 #include <stdint.h>
 
-
-typedef struct DataProc {
+typedef struct dataproc_t {
   uint8_t cond : 4;
   uint8_t is_immediate : 1;
   uint8_t opcode : 4;
@@ -11,9 +10,9 @@ typedef struct DataProc {
   uint8_t rn : 4;
   uint8_t rd : 4;
   uint16_t op2 : 12;
-} DataProc;
+} dataproc_t;
 
-typedef struct Multiply {
+typedef struct multiply_t {
   uint8_t cond : 4;
   uint8_t accumulate : 1;
   uint8_t set_cond : 1;
@@ -21,9 +20,9 @@ typedef struct Multiply {
   uint8_t rn : 4;
   uint8_t rs : 4;
   uint8_t rm : 4;
-} Multiply;
+} multiply_t;
 
-typedef struct SDT {
+typedef struct sdt_t {
   uint8_t cond : 4;
   uint8_t is_offset : 1;
   uint8_t is_preindexed : 1;
@@ -32,22 +31,28 @@ typedef struct SDT {
   uint8_t rn : 4;
   uint8_t rd : 4;
   uint16_t offset : 12;
-} SDT;
+} sdt_t;
 
-typedef struct Branch {
+typedef struct branch_t {
   uint8_t cond : 4;
   uint32_t offset : 24;
-} Branch;
+} branch_t;
+
+enum instr_type {
+  RAW, DATAPROC, MULTIPLY, SDT, BRANCH
+};
+
+union instr_data {
+  uint32_t raw_data;
+  dataproc_t dataproc;
+  multiply_t multiply;
+  sdt_t sdt;
+  branch_t branch;
+};
 
 typedef struct instruction_t {
-  enum instr_type {Raw, DataProc, Multiply, SDT, Branch};
-  union instr_data {
-    uint32_t raw_data;
-    struct DataProc dataproc;
-    struct Multiply multiply;
-    struct SDT sdt;
-    struct Branch branch;
-  };
+  enum instr_type tag;
+  union instr_data data;
 } instruction_t;
 
 typedef struct pipeline_t {
@@ -56,7 +61,7 @@ typedef struct pipeline_t {
   instruction_t *executed;
 } pipeline_t;
 
-typedef struct machine_state_t {
+typedef struct arm11_state_t {
   uint32_t main_memory[65535];
   uint32_t register_file[16];
   pipeline_t *pipeline;

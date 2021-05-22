c3811d5b24112a90810be2c10946f5c32611e856
Ashvin Arsakularatne <aa9220@ic.ac.uk>
2021-05-22 22:05:19 +0530

    refactor: reorders structs to optimise for memory

--- src/emulate.h
+++ src/emulate.h
@@ -3,39 +3,39 @@
 #include <stdint.h>
 
 typedef struct dataproc_t {
-  uint8_t cond : 4;
-  uint8_t is_immediate : 1;
-  uint8_t opcode : 4;
-  uint8_t set_cond : 1;
-  uint8_t rn : 4;
-  uint8_t rd : 4;
   uint16_t op2 : 12;
+  uint8_t cond : 4;
+  uint8_t rn : 4;
+  uint8_t opcode : 4;
+  uint8_t rd : 4;
+  uint8_t is_immediate : 1;
+  uint8_t set_cond : 1;
 } dataproc_t;
 
 typedef struct multiply_t {
   uint8_t cond : 4;
-  uint8_t accumulate : 1;
-  uint8_t set_cond : 1;
   uint8_t rd : 4;
   uint8_t rn : 4;
   uint8_t rs : 4;
   uint8_t rm : 4;
+  uint8_t accumulate : 1;
+  uint8_t set_cond : 1;
 } multiply_t;
 
 typedef struct sdt_t {
+  uint16_t offset : 12;
   uint8_t cond : 4;
+  uint8_t rn : 4;
+  uint8_t rd : 4;
   uint8_t is_offset : 1;
   uint8_t is_preindexed : 1;
   uint8_t up_bit : 1;
   uint8_t load : 1;
-  uint8_t rn : 4;
-  uint8_t rd : 4;
-  uint16_t offset : 12;
 } sdt_t;
 
 typedef struct branch_t {
-  uint8_t cond : 4;
   uint32_t offset : 24;
+  uint8_t cond : 4;
 } branch_t;
 
 enum instr_type { RAW, DATAPROC, MULTIPLY, SDT, BRANCH };
@@ -49,8 +49,8 @@ union instr_data {
 };
 
 typedef struct instruction_t {
-  enum instr_type tag;
   union instr_data data;
+  enum instr_type tag;
 } instruction_t;
 
 typedef struct pipeline_t {
@@ -60,7 +60,7 @@ typedef struct pipeline_t {
 } pipeline_t;
 
 typedef struct arm11_state_t {
-  uint32_t main_memory[65535];
+  uint8_t main_memory[65535];
   uint32_t register_file[16];
   pipeline_t *pipeline;
 } arm11_state_t;

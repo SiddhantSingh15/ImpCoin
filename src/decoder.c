7f401dd1a1f863a87c26612da85f07cb56312f7b
Ashvin Arsakularatne <aa9220@ic.ac.uk>
2021-05-24 18:48:04 +0530

    feat: add branch decoder and macros for bit extraction

--- src/decoder.c
+++ src/decoder.c
@@ -2,15 +2,25 @@
 #include <assert.h>
 #include <stdint.h>
 
-// TODO: Use macros instead of functions for extracting bits?
+#define EXTRACT_BITS(raw, from, size)                                          \
+  (uint32_t)(                                                                  \
+      ((((1 << (uint8_t)size) - 1) << (uint8_t)from) & (uint32_t)raw) >>       \
+      (uint8_t)from)
 
-uint32_t extract_bits(uint32_t raw, uint8_t from, uint8_t to) {
-  return ((((1 << to) - 1) << from) & raw) >> from;
+#define EXTRACT_BIT(raw, pos)                                                  \
+  (uint32_t)(((1 << (uint8_t)pos) & (uint8_t)raw) >> (uint8_t)pos)
+
+// TODO: Macros or functions?
+
+/*
+uint32_t extract_bits(uint32_t raw, uint8_t from, uint8_t size) {
+  return ((((1 << size) - 1) << from) & raw) >> from;
 }
 
 uint8_t extract_bit(uint32_t raw, uint8_t position) {
   return ((1 << position) & raw) >> position;
 }
+*/
 
 instruction_t *decode_dataproc(instruction_t *instr) {
   // the instruction to be decoded must be raw
@@ -18,13 +28,13 @@ instruction_t *decode_dataproc(instruction_t *instr) {
 
   uint32_t raw_data = instr->data.raw_data;
 
-  instr->data.dataproc.cond = extract_bits(raw_data, 28, 32);
-  instr->data.dataproc.is_immediate = extract_bit(raw_data, 25);
-  instr->data.dataproc.opcode = extract_bits(raw_data, 21, 25);
-  instr->data.dataproc.set_cond = extract_bit(raw_data, 21);
-  instr->data.dataproc.rn = extract_bits(raw_data, 16, 20);
-  instr->data.dataproc.rd = extract_bits(raw_data, 12, 16);
-  instr->data.dataproc.op2 = extract_bits(raw_data, 0, 12);
+  instr->data.dataproc.cond = EXTRACT_BITS(raw_data, 28, 4);
+  instr->data.dataproc.is_immediate = EXTRACT_BIT(raw_data, 25);
+  instr->data.dataproc.opcode = EXTRACT_BITS(raw_data, 21, 4);
+  instr->data.dataproc.set_cond = EXTRACT_BIT(raw_data, 21);
+  instr->data.dataproc.rn = EXTRACT_BITS(raw_data, 16, 4);
+  instr->data.dataproc.rd = EXTRACT_BITS(raw_data, 12, 4);
+  instr->data.dataproc.op2 = EXTRACT_BITS(raw_data, 0, 12);
 
   instr->tag = DATAPROC;
   return instr;
@@ -36,12 +46,12 @@ instruction_t *decode_multiply(instruction_t *instr) {
 
   uint32_t raw_data = instr->data.raw_data;
 
-  instr->data.multiply.cond = extract_bits(raw_data, 28, 32);
-  instr->data.multiply.accumulate = extract_bit(raw_data, 21);
-  instr->data.multiply.set_cond = extract_bit(raw_data, 20);
-  instr->data.multiply.rd = extract_bits(raw_data, 16, 20);
-  instr->data.multiply.rn = extract_bits(raw_data, 12, 16);
-  instr->data.multiply.rs = extract_bits(raw_data, 0, 12);
+  instr->data.multiply.cond = EXTRACT_BITS(raw_data, 28, 4);
+  instr->data.multiply.accumulate = EXTRACT_BIT(raw_data, 21);
+  instr->data.multiply.set_cond = EXTRACT_BIT(raw_data, 20);
+  instr->data.multiply.rd = EXTRACT_BITS(raw_data, 16, 4);
+  instr->data.multiply.rn = EXTRACT_BITS(raw_data, 12, 4);
+  instr->data.multiply.rs = EXTRACT_BITS(raw_data, 0, 12);
 
   instr->tag = MULTIPLY;
   return instr;
@@ -53,26 +63,32 @@ instruction_t *decode_sdt(instruction_t *instr) {
 
   uint32_t raw_data = instr->data.raw_data;
 
-  instr->data.sdt.cond = extract_bits(raw_data, 28, 32);
-  instr->data.sdt.is_offset = extract_bit(raw_data, 25);
-  instr->data.sdt.is_preindexed = extract_bit(raw_data, 24);
-  instr->data.sdt.load = extract_bit(raw_data, 20);
-  instr->data.sdt.rn = extract_bits(raw_data, 16, 20);
-  instr->data.sdt.rd = extract_bits(raw_data, 12, 16);
-  instr->data.sdt.offset = extract_bits(raw_data, 0, 12);
+  instr->data.sdt.cond = EXTRACT_BITS(raw_data, 28, 4);
+  instr->data.sdt.is_offset = EXTRACT_BIT(raw_data, 25);
+  instr->data.sdt.is_preindexed = EXTRACT_BIT(raw_data, 24);
+  instr->data.sdt.load = EXTRACT_BIT(raw_data, 20);
+  instr->data.sdt.rn = EXTRACT_BITS(raw_data, 16, 4);
+  instr->data.sdt.rd = EXTRACT_BITS(raw_data, 12, 4);
+  instr->data.sdt.offset = EXTRACT_BITS(raw_data, 0, 12);
 
   instr->tag = SDT;
   return instr;
 }
 
-/*
-instruction_t *decode_branch(instruction_t *raw) {
+instruction_t *decode_branch(instruction_t *instr) {
   // the instruction to be decoded must be raw
-  assert(raw->tag == RAW);
+  assert(instr->tag == RAW);
+
+  uint32_t raw_data = instr->data.raw_data;
+
+  instr->data.branch.cond = EXTRACT_BITS(raw_data, 28, 4);
+  instr->data.branch.offset = EXTRACT_BITS(raw_data, 0, 24);
+
+  instr->tag = BRANCH;
+  return instr;
 }
 
 instruction_t *decode(instruction_t *raw) {
   // the instruction to be decoded must be raw
   assert(raw->tag == RAW);
 }
-*/

df486408d02a6802c548563aefa6c70ba196b8c7
yelun <yly19@ic.ac.uk>
2021-05-28 19:04:33 +0800

    refactor: shifted testrunner.c out of testing folder

--- src/testrunner.c
+++ src/testrunner.c
@@ -1,8 +1,8 @@
-#include "../emulate_lib/decoder.h"
-#include "../global_helpers/types.h"
-#include "../emulate_lib/emulate_utils.h"
-#include "../emulate_lib/exec_utils.h"
-#include "test_utils.h"
+#include "emulate_lib/decoder.h"
+#include "global_helpers/types.h"
+#include "emulate_lib/emulate_utils.h"
+#include "emulate_lib/exec_utils.h"
+#include "testing/test_utils.h"
 #include <limits.h>
 #include <stdint.h>
 #include <stdio.h>

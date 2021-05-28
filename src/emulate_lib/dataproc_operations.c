3862fdb1090cf27f9dd8f666c0af2b3d38bf0d2a
Kavya Chopra <kc2320@ic.ac.uk>
2021-05-28 21:44:51 +0400

    feat : add helping function pointers for dataprocessing operations

---
+++ src/emulate_lib/dataproc_operations.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include "../global_helpers/definitions.h"
+#include "exec_utils.h"
+
+int32_t and(int32_t op1, int32_t op2){
+  return op1 & op2;
+}
+
+int32_t eor(int32_t op1, int32_t op2){
+  return op1 ^ op2;
+}
+
+int32_t sub(int32_t op1, int32_t op2, int *carry_out){
+  *carry_out = !overflow(op1, twos_comp(op2));
+  return op1 - op2;
+}
+
+int32_t rsb(int32_t op1, int32_t op2, int *carry_out){
+  *carry_out = !overflow(op2, twos_comp(op1));
+  return op2 - op1;
+}
+
+int32_t add(int32_t op1, int32_t op2, int *carry_out){
+  *carry_out = overflow(op1, op2);
+  return op1 + op2;
+}
+

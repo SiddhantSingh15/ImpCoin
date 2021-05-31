d434784864e945a043b5bd56755f0fb53eb0af06
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2021-05-31 16:17:01 +0300

    fix: adding ability to dynamically pass the instruction array size.

--- src/assemble_lib/assemble_utils.c
+++ src/assemble_lib/assemble_utils.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include "definitions.h"
 
-void write_file(uint32_t *instr_array, char *save_name) {
+void write_file(uint32_t *instr_array, int array_size, char *save_name) {
   FILE *file = fopen(save_name, "wb");
 
-  for (int i = 0; i < sizeof(instr_array); i++) {
+  for (int i = 0; i < array_size; i++) {
     fwrite(&instr_array[i], INSTR_SIZE, 1, file);
   }
 

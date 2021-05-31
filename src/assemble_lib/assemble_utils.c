db159eb9836172044f4f5fa7dfe72432473d4dba
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2021-05-31 16:14:19 +0300

    feat: adding the ability to write a binary file.

--- src/assemble_lib/assemble_utils.c
+++ src/assemble_lib/assemble_utils.c
@@ -1,2 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "definitions.h"
+
+void write_file(uint32_t *instr_array, char *save_name) {
+  FILE *file = fopen(save_name, "wb");
+
+  for (int i = 0; i < sizeof(instr_array); i++) {
+    fwrite(&instr_array[i], INSTR_SIZE, 1, file);
+  }
+
+  fclose(file);
+}

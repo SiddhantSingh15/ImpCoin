d7ca8e2be108657d101f4107567493abaf1dd09d
yelun <yly19@ic.ac.uk>
2021-05-31 20:29:46 +0800

    chore: create directories for symbol table utilities

---
+++ src/assemble_lib/symbol_table_utils.c
@@ -0,0 +1,3 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../global_helpers/types.h"
---
+++ src/assemble_lib/symbol_table_utils.h
@@ -0,0 +1,6 @@
+#ifndef SYMBOL_TABLE_UTILS_H
+#define SYMBOL_TABLE_UTILS_H
+
+#include "../global_helpers/types.h"
+
+#endif // SYMBOL_TABLE_UTILS_H

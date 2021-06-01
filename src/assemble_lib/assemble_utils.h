dc14453cfa7f4cab920f7a2cd18fe91c9d2a7eaf
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2021-06-01 11:48:10 +0300

    chore: added function description in `assemble_utils.h`.

--- src/assemble_lib/assemble_utils.h
+++ src/assemble_lib/assemble_utils.h
@@ -1,4 +1,16 @@
 #ifndef ASSEMBLE_UTILS_H
 #define ASSEMBLE_UTILS_H
 
-#endif // ASSEMBLUE_UTILS_H
+#include <stdlib.h>
+
+
+/**
+ * @brief Saves the instruction array into the specified binary file.
+ * 
+ * @param instr_array pointer to the instruction array.
+ * @param array_size size of the instruction array.
+ * @param save_name name of the new binary file to be made.
+ */
+void write_file(uint32_t *instr_array, int array_size, char *save_name);
+
+#endif /* ASSEMBLUE_UTILS_H */

02cbb0b6be97ad52e6e0d6acd552f6056903c765
Ashvin Arsakularatne <aa9220@ic.ac.uk>
2021-06-04 01:11:50 +0530

    feat: add basic structs for tokenizer

--- src/assemble_lib/tokens.h
+++ src/assemble_lib/tokens.h
@@ -5,27 +5,36 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct opcode_data_t {
-  enum instr_type type;
-  char *opcode;
-};
-
 union token_data {
-  struct opcode_data_t opcode_data;
+  char *instr_name;
   uint8_t reg;
   uint32_t exp;
   uint8_t shift_name;
   uint8_t cond;
-  char *address;
+  char separator;
+  char *label;
   char *error;
 };
 
-// TODO: break down address where?
-enum token_type { OPCODE, REG, EXPRESSION, SHIFTNAME, COND, ADDRESS, TOKERR };
+enum token_type {
+  INSTRNAME,
+  REG,
+  EXPRESSION,
+  SHIFTNAME,
+  COND,
+  SEPARATOR,
+  LABEL,
+  TOKERR
+};
 
 typedef struct token {
   union token_data data;
   enum token_type type;
 } token_t;
 
-#endif /* TOKENS_H*/
+typedef struct token_list {
+  struct token list[16];
+  uint8_t size;
+} token_list;
+
+#endif /* TOKENS_H */

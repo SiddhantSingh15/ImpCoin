29670239ed56e3851814ed40fa21f532437f7eb8
Kavya Chopra <kc2320@ic.ac.uk>
2021-06-10 17:38:09 +0400

    chore : add documentation to assemble_utils.h

--- src/assemble_lib/assemble_utils.h
+++ src/assemble_lib/assemble_utils.h
@@ -2,11 +2,11 @@
 #define ASSEMBLE_UTILS_H
 
 #include "../global_helpers/definitions.h"
-#include "symbol_table_utils.h"
 #include "linked_list.h"
+#include "symbol_table_utils.h"
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * @brief Saves the instruction array into the specified binary file.
@@ -16,16 +16,29 @@
  */
 void write_file(FILE *file, uint32_t *binary_instr);
 
+/**
+ * @brief Reads the given file and converts each line into a list of tokens and
+ * puts them into a linked list.
+ *
+ * @param filename The file to be read.
+ * @param instruction The linked list to which tokens must be added.
+ * @param symbols The table where we add labels-adress path.
+ */
 void read_asm(char *filename, linked_list *instructions, symbol_table *symbols);
 
+/**
+ * @brief Sets up the symbol table to contain instruction names. Each
+ * instruction name maps to a code and its parse function.
+ *
+ * @param st The symbol table that we are adding to.
+ */
 void populate_st_instructions(symbol_table *st);
-
 
 /**
  * @brief Checks if the token is a shift type.
- * 
+ *
  * @param token The token to check.
- * 
+ *
  * @return returns the relevant int8_t of the shift type from definitions.h.
  */
 int8_t is_shift(char *token);
